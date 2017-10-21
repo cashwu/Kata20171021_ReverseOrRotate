7e1583474c989a43a8daed8a81606f57928074ac
cash <cash@cashwu.com>
2017-10-21 21:27:44 +0800

    input empty and 0

--- Kata20171021_ReverseOrRotate/ReverseOrRotateTests.cs
+++ Kata20171021_ReverseOrRotate/ReverseOrRotateTests.cs
@@ -7,8 +7,19 @@ namespace Kata20171021_ReverseOrRotate
     public class ReverseOrRotateTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void input_empty_string_and_0_sz()
         {
+            var revrot = new Revrot();
+            var result = revrot.RevRot("", 0);
+            Assert.AreEqual("", result);
+        }
+    }
+
+    public class Revrot
+    {
+        public string RevRot(string strng, int i)
+        {
+            return strng;
         }
     }
 }
